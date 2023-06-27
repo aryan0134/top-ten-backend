2528585ab9d83f605b6fb8b6d0881224da5e8061
aryan0134 <aryan0134.be20@chitkara.edu.in>
2023-06-27 17:16:08 +0530

    updating

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const messageRoutes = require('./routes/messageRoutes')
 const cors = require("cors");
+const path = require('path')
+const root = require('./routes/root')
 
 // Express App
 
@@ -18,6 +20,8 @@ app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use(cors())
 
@@ -38,7 +42,19 @@ app.use((req, res, next) => {
 
 //Routes
 
+app.use('/', root)
 app.use('/api/messages' , messageRoutes)
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
 
 //connecting to MongoDB
 
